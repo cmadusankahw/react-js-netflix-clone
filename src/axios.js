1ed35bb3c28b248c335dcdac6b95107e6ede7334
Chiran Hewawitharana <chiranh@zone24x7.com>
2020-12-29 20:40:56 +0530

    added footer

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 // making requests to movie databse : base url
 const instance = axios.create({
-    baseURL: 'http://api.themoviedb.org/3',
+  baseURL: "https://api.themoviedb.org/3",
 });
 
 export default instance;
---
+++ src/comps/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer_fadeBottom"></div>
+      <div className="footer">
+        <p>
+          {" "}
+          Developed by &#169; <b>Chiran Hewawitharana. </b> 2020{" "}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
